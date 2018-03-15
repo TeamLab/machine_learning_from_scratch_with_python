c68bbfa1d78c2557fd5942d336eb8033f5c149b4
Sungchul Choi <sc82.choi@gachon.ac.kr>
2018-03-15 12:17:08 +0900

    update ch10 codes

---
+++ code/ch10/teamlab_classifier.py
@@ -0,0 +1,13 @@
+class SoftmaxRegressionClassifier(Object):
+
+    def softmax():
+        return value
+
+    def loss():
+        return loss
+
+    def fit():
+        return value
+
+    def predict():
+        return value
